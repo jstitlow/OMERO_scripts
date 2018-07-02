c2125d566724e469916d4d8c8ad4b9e934e6165b
Josh Titlow <jstitlow@gmail.com>
2018-07-02 12:19:53 +0100

    Update OMERO_export_dataset.py

--- OMERO_export_dataset.py
+++ OMERO_export_dataset.py
@@ -3,10 +3,18 @@ from omero.gateway import BlitzGateway
 import os
 import getpass
 import threading
+import time
 
 USER = os.environ['USER']
 PASS = getpass.getpass("Enter Password:")
 HOST = os.environ['HOST']
+
+dataset_id = 16822
+
+HOST="omero1.bioch.ox.ac.uk"
+conn = BlitzGateway(USER, PASS, host=HOST, port=4064)
+conn.connect()
+conn.SERVICE_OPTS.setOmeroGroup(-1)
 
 # keep connection to OMERO alive
 def keep_connection_alive():
@@ -17,13 +25,6 @@ def keep_connection_alive():
 th_ka = threading.Thread(target = keep_connection_alive)
 th_ka.daemon = True
 th_ka.start()
-
-dataset_id = 16977
-
-HOST="omero1.bioch.ox.ac.uk"
-conn = BlitzGateway(USER, PASS, host=HOST, port=4064)
-conn.connect()
-conn.SERVICE_OPTS.setOmeroGroup(-1)
 
 for image in conn.getObject("Dataset", dataset_id).listChildren():
 
