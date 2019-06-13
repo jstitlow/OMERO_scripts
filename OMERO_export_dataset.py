cb099ab2afa324d0505bf4480f3dc34ff6b09ea0
bioc1301 <joshua.titlow@bioch.ox.ac.uk>
2019-06-13 22:13:44 +0100

    export

--- OMERO_export_dataset.py
+++ OMERO_export_dataset.py
@@ -5,18 +5,19 @@ import getpass
 import threading
 import time
 
-# specify a dataset
-dataset_id = 16822
 
 # initialise OMERO
-USER = getpass.getpass("Enter username:")
-PASS = getpass.getpass("Enter Password:")
-HOST="omero1.bioch.ox.ac.uk"
+USER = getpass.getuser()
+print 'username: ', USER
+PASS = getpass.getpass('Enter password:')
+HOST='omero1.bioch.ox.ac.uk'
 
 conn = BlitzGateway(USER, PASS, host=HOST, port=4064)
 conn.connect()
 conn.SERVICE_OPTS.setOmeroGroup(-1)
 
+print 'login successful'
+dataset_id = input ('Enter dataset ID:')
 # keep connection to OMERO alive
 def keep_connection_alive():
     while True:
@@ -27,15 +28,15 @@ th_ka = threading.Thread(target = keep_connection_alive)
 th_ka.daemon = True
 th_ka.start()
 
-for image in conn.getObject("Dataset", dataset_id).listChildren():
+for image in conn.getObject('Dataset', dataset_id).listChildren():
 
     for orig_file in image.getImportedImageFiles():
 
         file_name = orig_file.getName()
-        print ("Downloading...", file_name)
+        print ('Downloading...', file_name)
 
-#    with open(file_name, "wb") as f:
-#        for chunk in orig_file.getFileInChunks(buf=2621440):
-#            f.write(chunk)
+    with open(file_name, 'wb') as f:
+        for chunk in orig_file.getFileInChunks(buf=2621440):
+            f.write(chunk)
 
 conn.close()
